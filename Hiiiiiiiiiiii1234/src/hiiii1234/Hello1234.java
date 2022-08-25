ccbaa01c0d2b7744b084433d36565e6f97a462eb
sapna <sapna@LAPTOP-M66F2DUF>
2022-08-25 22:44:41 +0530

    master branch

---
+++ Hiiiiiiiiiiii1234/src/hiiii1234/Hello1234.java
@@ -0,0 +1,27 @@
+package hiiii1234;
+
+public class Hello1234 {
+	
+	public static void main(String [] args)
+	{
+		int x = 20;
+		if(x==10)
+		{
+			System.out.println("Yes==10");
+		
+			if(x>5)
+			{
+				System.out.println("X is Greter than 5");
+			}
+		}
+				else
+				{
+					System.out.println("Exit");
+
+				}
+					
+				
+	}	
+	}
+	
+	
